441f9e2d75267f5179a8fbe5e01a84cf84713de5
Felipe Volkweis <felipe.volkweis@wr.sc.usp.br>
2026-03-14 22:08:45 -0300

    code: added base types from eigein

---
+++ src/types/mat4x4.h
@@ -0,0 +1,8 @@
+#ifndef MAT4X4_H
+#define MAT4X4_H
+
+#include <Eigen/Dense>
+
+using Mat4x4 = Eigen::Matrix<float, 4, 4>;
+
+#endif
---
+++ src/types/vec3.h
@@ -0,0 +1,8 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+#include <Eigen/Dense>
+
+using Vec3 = Eigen::Matrix<float, 3, 1>;
+
+#endif
